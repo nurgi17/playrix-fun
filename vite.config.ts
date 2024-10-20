bae26e0f57c363f9b239867bf7b9334fd6f062a8
nurgi17 <usen.nurgisa@gmail.com>
2024-10-20 13:15:38 +0500

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,37 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Playrix Fun',
+        short_name: 'Playrix Fun',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'path/to/icon.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'path/to/icon.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   base: '/playrix-fun/',
   resolve: {
     alias: {
